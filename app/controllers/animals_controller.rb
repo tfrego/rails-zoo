5709bf1207c3124a37124fe11ba40ad822ddaa8d
Trang <trang.frego@gmail.com>
2018-09-28 10:26:38 -0700

    add new and create action and form view

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -8,6 +8,23 @@ class AnimalsController < ApplicationController
   end
 
   def new
+    @animal = Animal.new
+  end
+
+  def create
+    @animal = Animal.new(animal_params)
+
+    if @animal.save
+      redirect_to animals_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def animal_params
+    return params.require(:animal).permit(:name, :species, :age)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get '/animals/new', to: 'animals#new'
+  get '/animals/new', to: 'animals#new', as: 'new_animal'
+  post '/animals', to: 'animals#create'
 
   get '/animals', to: 'animals#index'
   get '/animals/:id', to: 'animals#show', as: 'animal'
